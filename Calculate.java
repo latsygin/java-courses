47f2e1cb32f4fd4a6c29579a6df4044a16b2a54d
SergeyL <latsygin@mail.ru>
2017-10-10 10:37:41 +0300

    create Calculate

--- Calculate.java
+++ Calculate.java
@@ -1,5 +1,15 @@
 public class Calculate {
-	public static void main(String[] args){
+	public static void main(String[] arg){
 		System.out.println("Calculate...");
+		double first = Double.valueOf(arg[0]);
+		double second = Double.valueOf(arg[1]);
+		double sum = first + second;
+		double proizv = first * second;
+		double chast = first / second;
+		double razn = first - second;
+		System.out.println("Sum = " + sum);
+		System.out.println("Proizv = " + proizv);
+		System.out.println("Chastnoe = " + chast);
+		System.out.println("Raznost = " + razn);
 	}
 }
